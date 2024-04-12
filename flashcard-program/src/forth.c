57d67c28a4efb7e189eb83b4e550f7953a55fc6d
Nitya Guntaka <66152719+nitti02@users.noreply.github.com>
2024-04-12 14:41:55 -0500

    Update forth.c

---
+++ flashcard-program/src/forth.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "forth.h"
+
+struct ForthEnv {
+   
+};
+
+ForthEnv* forth_create_env() {
+ 
+}
+
+void forth_eval(ForthEnv* env, const char* code) {
+ 
+}
+
+void forth_destroy_env(ForthEnv* env) {
+
+}
