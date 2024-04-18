9f8d32fc0be3f3ac6cd9979acb51d8761a83b953
Kenny Van <119907786+kennyv22@users.noreply.github.com>
2024-04-18 01:40:19 +0000

    created a stack and dictionary to hold Forth words

--- flashcard-program/include/forth.h
+++ flashcard-program/include/forth.h
@@ -1,7 +1,21 @@
 #ifndef FORTH_H
 #define FORTH_H
 
-typedef struct ForthEnv ForthEnv;
+#define STACK_SIZE 256
+#define MAX_WORD_LENGTH 32
+#define DICTIONARY_SIZE 60
+
+typedef struct {
+    char name[MAX_WORD_LENGTH];
+    void (*function)(struct ForthEnv* env);
+} ForthWord;
+
+typedef struct {
+    int stack[STACK_SIZE];
+    int stackPointer;
+    ForthWord dictionary[DICTIONARY_SIZE];
+    int dictSize;
+} ForthEnv;
 
 ForthEnv* forth_create_env();
 void forth_eval(ForthEnv* env, const char* code);
