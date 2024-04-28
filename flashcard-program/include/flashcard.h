6788590f6acb93c1b47e3ab31d491c133ba0bbe8
Nitya Guntaka <66152719+nitti02@users.noreply.github.com>
2024-04-28 13:26:59 -0500

    Update flashcard.h

--- flashcard-program/include/flashcard.h
+++ flashcard-program/include/flashcard.h
@@ -1,9 +1,14 @@
 #ifndef FLASHCARD_H
 #define FLASHCARD_H
+
 #include "forth.h"
 
-typedef struct Flashcard Flashcard;
+typedef struct Flashcard {
+    char* question;
+    char* answer;
+} Flashcard;
 
+// Function declarations...
 Flashcard* flashcard_new(const char* question, const char* answer);
 void flashcard_delete(Flashcard* flashcard);
 void flashcard_display(const Flashcard* flashcard, ForthEnv* env);
